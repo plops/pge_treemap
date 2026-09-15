a7a1e01db9628a9e922eae2568cbf7918db5a052
wol <wolpumba@gmail.com>
2026-09-15 16:22:06 +0000

    collect script

--- collect.sh
+++ collect.sh
@@ -1,4 +1,4 @@
-for i in source0/{CMakeLists.txt,src/main.cpp} .github/workflows/*.yaml
+for i in source0/{CMakeLists.txt,src/main.cpp} .github/workflows/*.yml
 do
     echo "// start of "$i
     cat $i
